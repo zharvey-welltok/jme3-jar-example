7c10a1526b7357341c1ffef3edf5424d5769b858
Zac Harvey <zac.harvey@welltok.com>
2016-10-10 04:19:43 -0400

    Initial commit

---
+++ src/main/groovy/bimderboo/jmejarexample/Library.groovy
@@ -0,0 +1,14 @@
+package bimderboo.jmejarexample
+
+import com.jme3.app.SimpleApplication
+
+class Library extends SimpleApplication {
+    static void main(String[] args) {
+        new Library().start()
+    }
+
+	@Override
+	public void simpleInitApp() {
+		// TODO: No-op for now
+	}
+}
